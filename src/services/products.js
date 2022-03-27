4aba75d140bd3d1523a4e03aa8e465aa752ad753
Andre Barquet <andreluiz97.bm@gmail.com>
2022-03-27 12:40:57 -0300

    add request on utils

--- src/services/products.js
+++ src/services/products.js
@@ -1,13 +1,5 @@
-import axios from "axios";
+import { request } from "../utils/utils";
 
 export async function getAllProducts() {
-  return await axios.get('/product/all')
-    .then(function (response) {
-      // handle success
-      return response?.data;
-    })
-    .catch(function (error) {
-      // handle error
-      return null;
-    })
+  return await request({ method: 'get', url: '/product/all' })
 };
---
+++ src/utils/utils.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+axios.defaults.baseURL = 'http://localhost:8080';
+axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8';
+axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+
+export function request({ method, url, data, onSuccess, onError } = {}) {
+  if (notExists(method) || notExists(url)) return;
+  axios({ method, url, data })
+    .then(function (response) {
+      // handle success
+      return response?.data;
+    })
+    .catch(function (error) {
+      // handle error
+      return null;
+    });
+}
+
+export function exists(value) {
+  return value !== null && value !== undefined;
+}
+
+export function notExists(value) {
+  return value === null || value === undefined;
+}
